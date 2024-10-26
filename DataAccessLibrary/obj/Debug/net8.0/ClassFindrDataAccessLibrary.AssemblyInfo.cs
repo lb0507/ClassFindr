4ac05ae097eb6f808be8fff84e863497e7e52e42
Logan Alpha <logan.alpha@integrashare.com>
2024-10-26 04:08:20 -0500

    Linked some of the skeleton pages to be naturally linked to

--- DataAccessLibrary/obj/Debug/net8.0/ClassFindrDataAccessLibrary.AssemblyInfo.cs
+++ DataAccessLibrary/obj/Debug/net8.0/ClassFindrDataAccessLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassFindrDataAccessLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a689817922867ce7bb99409d282e7b8d8a36a341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa0c1b0facc5cb829997e0055599f19791fb62e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassFindrDataAccessLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassFindrDataAccessLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
