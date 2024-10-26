36f6c95b92ee0b6007939baf5362cca3b18e4815
Logan Alpha <logan.alpha@integrashare.com>
2024-10-26 02:54:08 -0500

    Various changes

--- BlazorAPITest/obj/Debug/net8.0/BlazorAPITest.AssemblyInfo.cs
+++ BlazorAPITest/obj/Debug/net8.0/BlazorAPITest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84adb506135ac3571dd96effe033a12bf502a54a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223b96bb37dcc71522ae632171a7ae84a7acad99")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
