c81407778debdc2fef005889e58bf6ceddd121f6
BigBruhther <loganalpha2004@gmail.com>
2024-10-27 01:06:04 -0500

    Added Leaflet to the project and made a basic interactable map on the home page.

--- BlazorAPITest/obj/Debug/net8.0/BlazorAPITest.AssemblyInfo.cs
+++ BlazorAPITest/obj/Debug/net8.0/BlazorAPITest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172ba65384d85b51a853b1a96797dcdc1aaa351e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0127fef88d8e50a45a014414d7a5c9629abae29")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAPITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
