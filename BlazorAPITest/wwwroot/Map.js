df0211156ecff72b0ce98a8b27562c1fe0eb2d54
Logan Alpha <logan.alpha@integrashare.com>
2024-11-04 23:58:18 -0600

    Published website and changed OpenTopoMap to Esri Satellite tiles

--- BlazorAPITest/wwwroot/Map.js
+++ BlazorAPITest/wwwroot/Map.js
@@ -17,13 +17,9 @@ var openMapsLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
 );
 
 // Satellite tile set
-var satellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}',
-    {
-        maxZoom: 20,
-        attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'jpg'
-    }
-);
+var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 
 
 // Loads the map
